14ee21ca8bff78b571bc8e9cb286bdb7f46e0572
licc <lichangchou_lcc@mail.qzkeji.cn>
2020-06-10 20:22:21 +0800

    完成part3课程内容

---
+++ part3/course/backend/index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+]
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(data => data.id)) : 0
+  return maxId + 1
+}
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
+})
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+  notes = notes.concat(note)
+
+  res.json(note)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = +req.params.id
+  const note = notes.find(data => data.id === id)
+
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+app.delete('/api/notes/:id', (req, res) => {
+  const id = +req.params.id
+  const index = notes.findIndex(data => data.id === id)
+  if (index != -1) {
+    notes.splice(index, 1)
+    res.status(204).end()
+  } else {
+    res.status(404).end()
+  }
+})
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
