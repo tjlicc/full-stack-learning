5c700bb976ad80a1ba5219b8d73723cdddeeefd9
licc <lichangchou_lcc@mail.qzkeji.cn>
2020-06-19 16:51:47 +0800

    完成part4-b的课程

--- part4/course/backend/controllers/notes.js
+++ part4/course/backend/controllers/notes.js
@@ -1,39 +1,42 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (req, res) => {
-  Note.find().then((notes) => res.json(notes))
+notesRouter.get('/', async (req, res) => {
+  const notes = await Note.find()
+  res.json(notes)
 })
-notesRouter.get('/:id', (req, res, next) => {
-  Note.findById(req.params.id).then(note => {
-    if (note) {
-      res.json(note)
-    } else {
-      res.status(404).end()
-    }
-  }).catch(err => next(err))
+notesRouter.get('/:id', async (req, res) => {
+  const note = await Note.findById(req.params.id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
 })
-notesRouter.post('/', (req, res, next) => {
+notesRouter.post('/', async (req, res) => {
   const body = req.body
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date()
   })
-  note.save().then(savedNote => res.json(savedNote)).catch(err => next(err))
+
+  const savedNote = await note.save()
+  res.json(savedNote)
 })
-notesRouter.delete('/:id', (req, res, next) => {
-  Note.findByIdAndRemove(req.params.id).then(() => {
-    res.status(204).end
-  }).catch(err => next(err))
+notesRouter.delete('/:id', async (req, res) => {
+  await Note.findByIdAndRemove(req.params.id)
+  res.status(204).end()
 })
-notesRouter.put('/:id', (req, res, next) => {
+notesRouter.put('/:id', async (req, res) => {
   const body = req.body
   const note = {
     content: body.content,
     important: body.important,
   }
-  Note.findByIdAndUpdate(req.params.id, note, { new: true }).then(updatedNote => res.json(updatedNote)).catch(err => next(err))
+
+  const updatedNote = await Note.findByIdAndUpdate(req.params.id, note, { new: true })
+  res.json(updatedNote)
 })
 
 module.exports = notesRouter
--- part4/course/backend/utils/config.js
+++ part4/course/backend/utils/config.js
@@ -2,5 +2,5 @@ require('dotenv').config()
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  MONGODB_URI: process.env.MONGODB_URI
+  MONGODB_URI: process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
 }
