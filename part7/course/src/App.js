4ca5061d31cefc361d9cd906bf0f563dee01ebd2
licc <lichangchou_lcc@mail.qzkeji.cn>
2020-08-14 23:12:57 +0800

    使用bootstrap

--- part7/course/src/App.js
+++ part7/course/src/App.js
@@ -69,6 +69,7 @@ import {
   Switch, Route, Link, Redirect,
   useHistory, useRouteMatch
 } from 'react-router-dom'
+import { Table, Form, Button, Alert, Nav, Navbar } from 'react-bootstrap'
 
 const Home = () => {
   return (
@@ -93,13 +94,18 @@ const Notes = ({ notes }) => {
   return (
     <div>
       <h2>Notes</h2>
-      <ul>
-        {notes.map(note =>
-          <li key={note.id}>
-            <Link to={`/notes/${note.id}`}>{note.content}</Link>
-          </li>
-        )}
-      </ul>
+      <Table striped>
+        <tbody>
+          {notes.map(note =>
+            <tr key={note.id}>
+              <td>
+                <Link to={`/notes/${note.id}`}>{note.content}</Link>
+              </td>
+              <td>{note.user}</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
     </div>
   )
 }
@@ -129,15 +135,15 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username: <input />
-        </div>
-        <div>
-          password: <input type='password' />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>username:</Form.Label>
+          <Form.Control type="text" name="username"></Form.Control>
+          <Form.Label>password:</Form.Label>
+          <Form.Control type="password" ></Form.Control>
+          <Button variant="primary" type="submit">login</Button>
+        </Form.Group>
+      </Form>
     </div>
   )
 }
@@ -145,19 +151,19 @@ const Login = (props) => {
 const App = () => {
   const [notes, setNotes] = useState([
     {
-      id: 1,
+      id: "1",
       content: 'HTML is easy',
       important: true,
       user: 'Matti Luukkainen'
     },
     {
-      id: 2,
+      id: "2",
       content: 'Browser can execute only Javascript',
       important: false,
       user: 'Matti Luukkainen'
     },
     {
-      id: 3,
+      id: "3",
       content: 'Most important methods of HTTP-protocol are GET and POST',
       important: true,
       user: 'Arto Hellas'
@@ -165,9 +171,14 @@ const App = () => {
   ])
 
   const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
 
   const login = (user) => {
     setUser(user)
+    setMessage(`welcome ${user}`)
+    setTimeout(() => {
+      setMessage(null)
+    }, 10000)
   }
 
   const padding = {
@@ -178,16 +189,34 @@ const App = () => {
   const note = match ? notes.find(item => item.id === match.params.id) : null
 
   return (
-    <div>
-      <div>
-        <Link style={padding} to="/">home</Link>
-        <Link style={padding} to="notes">notes</Link>
-        <Link style={padding} to="users">users</Link>
-        {user
-          ? <em>{user} logged in</em>
-          : <Link style={padding} to="/login">login</Link>
-        }
-      </div>
+    <div className="container">
+      {
+        (message &&
+          <Alert variant="success">{message}</Alert>
+        )
+      }
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/">home</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/notes">notes</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/users">users</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              {user
+                ? <em>{user} logged in</em>
+                : <Link to="/login">login</Link>
+              }
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
       <Switch>
         <Route path="/notes/:id">
